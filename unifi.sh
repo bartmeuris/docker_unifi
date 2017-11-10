01f996a38937973dc01b198f1a4ae759604d16ce
Bart Meuris <bart.meuris@gmail.com>
2017-11-10 10:28:05 +0000

    Fix permissions mess

--- unifi.sh
+++ unifi.sh
@@ -7,8 +7,7 @@ fi
 
 echo "Fixing permissions..."
 chown -R ${UNIFI_USER:-unifi} /var/lib/unifi/
-chown -R ${UNIFI_USER:-unifi} /usr/lib/unifi/
-chmod -R 0664 /var/lib/unifi/*
+chmod -R u+Xwr,g+Xwr,o+Xr /var/lib/unifi/ /usr/lib/unifi/
 echo "Starting Unifi controller..."
 cd /usr/lib/unifi
 exec su --preserve-environment --command "/usr/bin/java -Xmx${JAVA_XMX:-1024M} -jar /usr/lib/unifi/lib/ace.jar $*" ${UNIFI_USER:-unifi}
